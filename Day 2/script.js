67f594aa24b9658b0590b56fd6d7665fedeb7b04
sabita <sabitakhadka091@gmail.com>
2023-10-02 14:18:58 +0545

    new files added with js, html and css

---
+++ Day 2/script.js
@@ -0,0 +1,31 @@
+// 1. Ways to print in JavaScript
+console.log("Hello World");
+// alert("Hello");
+//document.write("Hey, This is document.write");
+// 2. Javascript console API
+console.warn("this is warning");
+console.error("this is an error");
+console.assert(4 == 7);
+
+// 3. JavaScript Variables
+//What are Variables? - Containers to store data values
+let number1 = 34;
+var number2 = 56;
+const number3 = 10;
+console.log(number1 + number2 + number3);
+
+// 4. Data types in JavaScript
+var stri = "This is a string";
+var str2 ='This is also a string'; 
+
+// objects
+const marks = {
+    ravi: 34, 
+    shubham: 78, 
+    harry: 99.977
+}
+    console.log (marks);
+    // Booleans
+var a = true;
+var b = false;
+console.log(a,b)
